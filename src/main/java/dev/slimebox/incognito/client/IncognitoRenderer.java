5af83c2a98479ee8428beb92fef50f045cf05b3a
Curle <curle@gemwire.uk>
2022-01-28 16:21:46 +0000

    Streaming Indicator Render

--- src/main/java/dev/slimebox/incognito/client/IncognitoRenderer.java
+++ src/main/java/dev/slimebox/incognito/client/IncognitoRenderer.java
@@ -1,9 +1,11 @@
 package dev.slimebox.incognito.client;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.*;
 import dev.slimebox.incognito.Incognito;
 import dev.slimebox.incognito.IncognitoState;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.renderer.GameRenderer;
 import net.minecraftforge.client.gui.ForgeIngameGui;
 
 /**
@@ -16,8 +18,7 @@ public class IncognitoRenderer {
         if (IncognitoState.ENABLED) {
             mStack.pushPose();
             RenderSystem.setShaderTexture(0, Incognito.byMod("textures/gui/streaming.png"));
-            // x, y, u, v, w, h
-            gui.blit(mStack, width - 16, 0, 0, 0, 16, 16);
+            GuiComponent.blit(mStack, width -32, 0, 0, 0, 32, 32, 32, 32);
             mStack.popPose();
         }
     }
